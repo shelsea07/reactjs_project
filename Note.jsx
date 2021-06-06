e9e1eae4b374d7e1b0ff1823283ce1046cc61dc1
shelsea07 <84846521+shelsea07@users.noreply.github.com>
2021-06-06 17:58:14 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Full-Stack Web Dev Training and Internship</h1>
+      <p>
+      The goal of this Training and Internship Program is to equip interns with the unique skills they need to build database-backed APIs and web applications with the proper industry level front-end as well as scale up the deployment for the millions. A graduate of this program will be able to design and build databases for software applications, create and deploy database-backed web APIs, and secure and manage user authentication and access control for an application backend. They will be able to build production level frontend according to the industrial standards. Students will also learn how to deploy a the entire web application to the cloud using Docker and Kubernetes and how to maintain and scale them.
+
+
+      </p>
+    </div>
+  );
+}
+export default Note;
